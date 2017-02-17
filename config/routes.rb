8168637be14d170b842d463d9c1aba02d05a7cb0
layoutschool <contact@layoutschool.com>
2017-02-17 09:12:42 +0000

    add the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
-
+  
+  get "pages/about" => "pages#about"
+  match "about", :to => "pages#about", via: :get
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
