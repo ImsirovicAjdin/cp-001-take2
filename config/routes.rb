bd1f74ac2ec4f6842ac3e87ebdf682d31e78b419
layoutschool <contact@layoutschool.com>
2017-02-18 23:04:54 +0000

    Adding a Test Task Page...
    Improve the look
    
    in app/controllers/pages_controller.rb,
    
    make a new page:
    
    def test
    end
    
    open app/config/routes.rb:
    
    get 'test' => 'pages#test'
    
    in browser <site>/test
    
    error "Template is missing" (Rails can't find the view)
    
    in app/views/pages/test.html.erb:
    
    <h1>Hello World</h1>
    
    we verified that it works, so now delete the h1.
    
    add Bootstrap content:
    
    <div class="row">
      <div class="col-md-4">
        <h1 class="text-center">To Do</h1>
    
        <div class="panel panel-info">
          <div class="panel-heading">Heading</div>
          <div class="panel-body">Content</div>
          <div class="panel-footer text-center">Footer</div>
        </div>
    
      </div>
      <div class="col-md-4">
        <h1 class="text-center">Doing</h1>
      </div>
      <div class="col-md-4 text-center">
        <h1 class="text-center">Done</h1>
      </div>
    </div>

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   get "pages/about" => "pages#about"
   match "about", :to => "pages#about", via: :get
   
+  get "pages/test" => "pages#test"
+  match "test", :to => "pages#test", via: :get
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
